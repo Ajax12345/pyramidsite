bcfb97170d1e51a33be4eed554d223d85e96b1b1
Ajax1234 <pentester18@gmail.com>
2017-10-06 11:03:21 -0400

    Update studentsite.py

--- studentsite.py
+++ studentsite.py
@@ -3,6 +3,7 @@ import colander
 import deform
 from werkzeug.datastructures import OrderedMultiDict
 import peppercorn
+import js.deform
 
 #Deriving from flask.Request
 class theRequest(flask.Request):
@@ -19,31 +20,44 @@ first_name = colander.SchemaNode(colander.String(),
             widget = text_input)
 
 '''
+css_widget = deform.widget.TextInputWidget(css_class="deform-widget-with-style")
 
 class dataList(colander.SequenceSchema):
 	item = simpleRecord()
 
 class topList(colander.MappingSchema):
 	items = dataList()
+'''
+class Schema(colander.Schema):
+            text = colander.SchemaNode(colander.String(),
+                                       validator=colander.Length(max=100),
+                                       widget=css_widget,
+                                       description='Enter some text')
+'''
 
 class Mapping(colander.Schema):
-	Firstname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
-	Lastname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
-	Email = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
+	#Firstname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
+	#Lastname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
+	#Email = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
+	Firstname = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+	Email = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+	Lastname = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+
 
 
 	date = colander.SchemaNode(colander.Date(), widget = deform.widget.DatePartsWidget(), description = "content date")
 
 class Schema(colander.Schema):
-	Age = colander.SchemaNode(colander.Integer(), css_class='deform-widget-with-style')
-	Firstname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
-	Lastname = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
-	Email = colander.SchemaNode(colander.String(), css_class='deform-widget-with-style')
+	Age = colander.SchemaNode(colander.Integer(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+	Firstname = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+	Lastname = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
+	Email = colander.SchemaNode(colander.String(), validator=colander.Length(min = 5, max=100), widget=css_widget, description='Enter some text')
 
 #	Number = colander.SchemaNode(colander.Integer())
 
 	#mapping = Mapping(widget = deform.widget.MappingWidget(template="mapping_accordion", open=True))
-
+class Schema1(colander.Schema):
+	password = colander.SchemaNode(colander.String(), validator=colander.Length(min=5), widget=deform.widget.CheckedPasswordWidget(redisplay=True), description='Type your password and confirm it')
 #Creating the flask "application"
 app = flask.Flask(__name__)
 
@@ -70,8 +84,9 @@ def index():
 	else:
 		#Just return an empty form
 
-		schema = Schema()
-		form = deform.Form(schema, buttons=('submit',)).render()
+		schema = Schema1()
+		form = deform.Form(schema, buttons=('submit',)).render() #used to be	form = deform.Form(schema, buttons=('submit',)).render()
+
 		return flask.render_template("index.html", theForm = form, theMessage = "HI")
 
 
@@ -79,6 +94,10 @@ def index():
 @app.route("/")
 def home_page():
     return flask.render_template('home.html')
+
+@app.route("/get_skills", methods=["POST","GET"])
+def skils():
+	return flask.render_template("skills.html")
 
 if __name__=="__main__":
 	app.debug = True;
